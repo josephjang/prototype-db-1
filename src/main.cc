b6f165f369533d2038eb35e2f365e973908116e5
Jeongho Jang <josephjang@gmail.com>
2019-11-15 22:10:36 +0900

    Add spdlog

--- src/main.cc
+++ src/main.cc
@@ -1,12 +1,11 @@
 #include <iostream>
+#include "spdlog/spdlog.h"
 
-auto sum(int a, int b)
-{
-    return a + b;
-}
+auto sum(int a, int b) { return a + b; }
 
-int main()
-{
-    std::cout << "Hello, world!" << std::endl;
-    return 0;
+int main() {
+  std::cout << "Hello, world!" << std::endl;
+  spdlog::info("Hello, spdlog!");
+
+  return 0;
 }
