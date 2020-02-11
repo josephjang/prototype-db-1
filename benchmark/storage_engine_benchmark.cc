c0237ddee162b833ed4fd58beb18c4ab56321ff8
Jeongho Jang <josephjang@gmail.com>
2020-02-12 00:00:10 +0900

    Add benchmarks for abseil::flat_hash_map and libcuckoo

--- benchmark/storage_engine_benchmark.cc
+++ benchmark/storage_engine_benchmark.cc
@@ -1,6 +1,8 @@
 #include <benchmark/benchmark.h>
 
 #include "storage_engine.h"
+#include "libcuckoo/cuckoohash_map.hh"
+#include "absl/container/flat_hash_map.h"
 
 namespace protodb1 {
 
@@ -22,47 +24,153 @@ class StorageEngineBenchmark : public benchmark::Fixture {
   std::vector<std::string> keys_;
 };
 
-BENCHMARK_DEFINE_F(StorageEngineBenchmark, StdUnorderedMapGet)(benchmark::State& state) {
+BENCHMARK_DEFINE_F(StorageEngineBenchmark, StdUnorderedMapSet)(benchmark::State& state) {
   std::unordered_map<std::string, std::string> map;
   if (state.thread_index == 0) {
     SetUpKeys();
   }
+  
+
+  for (auto _ : state) {
+    for (int i = 0; i < 100000; i++) {
+      map.insert_or_assign(keys_[i], keys_[i]);
+    }
+  }
+
+  state.SetItemsProcessed(state.iterations() * num_keys_);
+}
+BENCHMARK_REGISTER_F(StorageEngineBenchmark, StdUnorderedMapSet)->UseRealTime()->Unit(benchmark::kMillisecond);
+
+BENCHMARK_DEFINE_F(StorageEngineBenchmark, StdUnorderedMapGet)(benchmark::State& state) {
+  std::unordered_map<std::string, std::string> map;
+  if (state.thread_index == 0) {
+    SetUpKeys();
+    for (int i = 0; i < 100000; i++) {
+      map.insert_or_assign(keys_[i], keys_[i]);
+    }
+  }
+
+  for (auto _ : state) {
+    for (int i = 0; i < 100000; i++) {
+      auto it = map.find(keys_[i]);
+      if (it != map.end()) {
+        *it;
+      }
+    }
+  }
+
+  state.SetItemsProcessed(state.iterations() * num_keys_);
+}
+BENCHMARK_REGISTER_F(StorageEngineBenchmark, StdUnorderedMapGet)->UseRealTime()->Unit(benchmark::kMillisecond);
+
+BENCHMARK_DEFINE_F(StorageEngineBenchmark, CuckoohashMapSet)(benchmark::State& state) {
+  libcuckoo::cuckoohash_map<std::string, std::string> map;
+  if (state.thread_index == 0) {
+    SetUpKeys();
+  }
+
+  for (auto _ : state) {
+    for (int i = 0; i < 100000; i++) {
+      map.insert_or_assign(keys_[i], keys_[i]);
+    }
+  }
+  
+  state.SetItemsProcessed(state.iterations() * num_keys_);
+}
+BENCHMARK_REGISTER_F(StorageEngineBenchmark, CuckoohashMapSet)->UseRealTime()->Unit(benchmark::kMillisecond);
+
+BENCHMARK_DEFINE_F(StorageEngineBenchmark, CuckoohashMapGet)(benchmark::State& state) {
+  libcuckoo::cuckoohash_map<std::string, std::string> map;
+  if (state.thread_index == 0) {
+    SetUpKeys();
+    for (int i = 0; i < 100000; i++) {
+      map.insert_or_assign(keys_[i], keys_[i]);
+    }
+  }
+
   for (auto _ : state) {
     for (int i = 0; i < 100000; i++) {
       map.find(keys_[i]);
     }
   }
+  
   state.SetItemsProcessed(state.iterations() * num_keys_);
 }
-BENCHMARK_REGISTER_F(StorageEngineBenchmark, StdUnorderedMapGet)->Threads(12);
+BENCHMARK_REGISTER_F(StorageEngineBenchmark, CuckoohashMapGet)->UseRealTime()->Unit(benchmark::kMillisecond);
 
-BENCHMARK_DEFINE_F(StorageEngineBenchmark, BM_Get)(benchmark::State& state) {
-  protodb1::StorageEngine storage_engine;
+BENCHMARK_DEFINE_F(StorageEngineBenchmark, AbseilFlatHashMapSet)(benchmark::State& state) {
+  absl::flat_hash_map<std::string, std::string> map;
   if (state.thread_index == 0) {
     SetUpKeys();
   }
+
   for (auto _ : state) {
     for (int i = 0; i < 100000; i++) {
-      storage_engine.Get(keys_[i]);
+      map.insert_or_assign(keys_[i], keys_[i]);
     }
   }
+  
   state.SetItemsProcessed(state.iterations() * num_keys_);
 }
-BENCHMARK_REGISTER_F(StorageEngineBenchmark, BM_Get)->Threads(12);
+BENCHMARK_REGISTER_F(StorageEngineBenchmark, AbseilFlatHashMapSet)->UseRealTime()->Unit(benchmark::kMillisecond);
+
+BENCHMARK_DEFINE_F(StorageEngineBenchmark, AbseilFlatHashMapGet)(benchmark::State& state) {
+  absl::flat_hash_map<std::string, std::string> map;
+  if (state.thread_index == 0) {
+    SetUpKeys();
+    for (int i = 0; i < 100000; i++) {
+      map.insert_or_assign(keys_[i], keys_[i]);
+    }
+  }
+
+  for (auto _ : state) {
+    for (int i = 0; i < 100000; i++) {
+      auto it = map.find(keys_[i]);
+      if (it != map.end()) {
+        *it;
+      }
+    }
+  }
+  
+  state.SetItemsProcessed(state.iterations() * num_keys_);
+}
+BENCHMARK_REGISTER_F(StorageEngineBenchmark, AbseilFlatHashMapGet)->UseRealTime()->Unit(benchmark::kMillisecond);
 
 BENCHMARK_DEFINE_F(StorageEngineBenchmark, BM_Set)(benchmark::State& state) {
   protodb1::StorageEngine storage_engine;
   if (state.thread_index == 0) {
     SetUpKeys();
   }
+
   for (auto _ : state) {
     for (int i = 0; i < 100000; i++) {
       storage_engine.Set(keys_[i], keys_[i]);
     }
   }
+
   state.SetItemsProcessed(state.iterations() * num_keys_);
 }
-BENCHMARK_REGISTER_F(StorageEngineBenchmark, BM_Set)->Threads(12);
+BENCHMARK_REGISTER_F(StorageEngineBenchmark, BM_Set)->UseRealTime()->Unit(benchmark::kMillisecond);
+
+
+BENCHMARK_DEFINE_F(StorageEngineBenchmark, BM_Get)(benchmark::State& state) {
+  protodb1::StorageEngine storage_engine;
+  if (state.thread_index == 0) {
+    SetUpKeys();
+    for (int i = 0; i < 100000; i++) {
+      storage_engine.Set(keys_[i], keys_[i]);
+    }
+  }
+  
+  for (auto _ : state) {
+    for (int i = 0; i < 100000; i++) {
+      storage_engine.Get(keys_[i]);
+    }
+  }
+
+  state.SetItemsProcessed(state.iterations() * num_keys_);
+}
+BENCHMARK_REGISTER_F(StorageEngineBenchmark, BM_Get)->UseRealTime()->Unit(benchmark::kMillisecond);
 
 BENCHMARK_DEFINE_F(StorageEngineBenchmark, BM_GetAndSet)
 (benchmark::State& state) {
@@ -70,17 +178,19 @@ BENCHMARK_DEFINE_F(StorageEngineBenchmark, BM_GetAndSet)
   if (state.thread_index == 0) {
     SetUpKeys();
   }
+  
   for (auto _ : state) {
-    for (int i = 0; i < 100000; i++) {
-      storage_engine.Get(keys_[i]);
-    }
     for (int i = 0; i < 100000; i++) {
       storage_engine.Set(keys_[i], keys_[i]);
     }
+    for (int i = 0; i < 100000; i++) {
+      storage_engine.Get(keys_[i]);
+    }
   }
+  
   state.SetItemsProcessed(state.iterations() * num_keys_ * 2);
 }
-BENCHMARK_REGISTER_F(StorageEngineBenchmark, BM_GetAndSet)->Threads(12);
+BENCHMARK_REGISTER_F(StorageEngineBenchmark, BM_GetAndSet)->UseRealTime()->Unit(benchmark::kMillisecond);
 
 }  // namespace protodb1
 
